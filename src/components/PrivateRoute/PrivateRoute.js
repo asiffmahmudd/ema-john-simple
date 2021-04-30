1664b79202779a40d92ff84db208a4b83be24dd9
Asif Mahmud <asifmahmud3472@gmail.com>
2021-04-30 23:37:04 +0600

    added env file

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,6 +1,5 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Redirect, Route } from 'react-router';
-import { UserContext } from '../../App';
 import { useAuth } from '../../Context/AuthContext';
 
 const PrivateRoute = ({ children, ...rest }) => {
@@ -9,7 +8,7 @@ const PrivateRoute = ({ children, ...rest }) => {
         <Route
             {...rest}
             render={({ location }) =>
-            loggedInUser?.email ? (
+            loggedInUser ? (
                 children
             ) : (
                 <Redirect
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,15 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
+console.log(process.env);
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyD6LqvHMGee51jK3hO75zAWAsfJfs9QjG0",
-    authDomain: "ema-john-10238.firebaseapp.com",
-    projectId: "ema-john-10238",
-    storageBucket: "ema-john-10238.appspot.com",
-    messagingSenderId: "668907978605",
-    appId: "1:668907978605:web:6977d5f3a7a13ce84aa464"
-  })
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+})
 
-  export const auth = app.auth()
-  export default app
+export const auth = app.auth()
+export default app
